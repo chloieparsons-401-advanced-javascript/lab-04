069b138ebbade03ca2479a050b0d49a2c586857c
Chloie Parsons <chloieparsons@gmail.com>
2019-05-30 10:30:41 -0700

    Added Fs mock file, repaired index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
+
 'use strict';
-
 const fs = require('fs');
-
 let arr = `'use strict'; \n 
 let arr = ['JP', 'Chloie', 'JT']; \n 
 arr.forEach(element => {
   console.log(element); \n
 });`
   .split('');
-
 let writeFile = binaryFile => { 
   return new Promise((resolve, reject) => { 
     fs.writeFile('./files/loop.js', binaryFile, (err) => {
@@ -16,20 +14,16 @@ let writeFile = binaryFile => {
     });
   });
 };
-
 let newBuffer = (arr) => {
   return new Promise((resolve, reject) => { 
     let data = Buffer.from('');
-
     arr.forEach(element => {
     data = Buffer.concat([data, Buffer.from(element)]);
       console.log(data);
     });
-
     resolve(data); 
   });
 };
-
 newBuffer(arr)
   .then(data => {
     writeFile(data);
@@ -46,9 +40,3 @@ let readFile = () => {
     console.log(readFile());
   });
 };
-
-
-
-
-  
-
